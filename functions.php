c3975e4cbf49200637888ad4586de091ec40f97b
Dmitry Christie <dmitry.christie@gmail.com>
2020-05-07 16:17:27 +0200

    disabled content editor in pages with flexible con

--- functions.php
+++ functions.php
@@ -226,3 +226,16 @@ function revealid_id_column_content( $column, $id ) {
     echo $id;
   }
 }
+
+add_action( 'admin_init', 'hide_editor' );
+ 
+function hide_editor() {
+    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+    if( !isset( $post_id ) ) return;
+ 
+    $template_file = get_post_meta($post_id, '_wp_page_template', true);
+     
+    if($template_file == 'flexible-content.php'){ // edit the template name
+        remove_post_type_support('page', 'editor');
+    }
+}
