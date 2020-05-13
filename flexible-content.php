5bd1a6adf6282da374d094b3e13ddb87ed4b2fdc
Dmitry Christie <dmitry.christie@gmail.com>
2020-05-13 14:27:41 +0200

    updated shortcode

--- flexible-content.php
+++ flexible-content.php
@@ -471,7 +471,7 @@ header'); if($header) { ?><h6 class="blurbs-header txt-center sm-red-line space"
                     ?>
                     </div>
                     load more
-                    <?php echo apply_filters( 'the_content', '[ajax_load_more container_type="div" posts_per_page="6" css_classes="the-loop" post_type="post" category="cyprus-news,guides,malta-news,market-outlook,portugal-news,property-market,spain-news,united-kingdom-news" offset="6" pause="true" scroll="false" button_label="Older Entries"]'); ?>
+                    <?php echo apply_filters( 'the_content', '[ajax_load_more container_type="div" posts_per_page="6" css_classes="the-loop"  offset="6" pause="true" scroll="false" button_label="Older Entries"]'); ?>
                     load more
 
             <!-- END Blog posts loop -->
