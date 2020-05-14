201a0030c74b5c942b1600b81d6928435110091f
Dmitry Christie <dmitry.christie@gmail.com>
2020-05-14 16:15:02 +0200

    footer updated

--- footer.php
+++ footer.php
@@ -23,21 +23,40 @@
 			<div class="footer-widgets container">
 				<div class="address">
 					<img src="<?php echo get_template_directory_uri(); ?>/img/footer-offices.png">
-					<h6>Our Offices</h6>
+
+					<?php the_field('offices', 'options'); ?>
+					<!--<h6>Our Offices</h6>
 					<ul class="footer-list">
 						<li><span>United Kingdom</span></li>
 						<li><span>Portugal</span></li>
 						<li><span>Hong Kong</span></li>
 						<li><span>Brazil</span></li>
-					</ul>
+					</ul> -->
 				</div>
 				<div class="menu">
 					<img class="hidden" src="<?php echo get_template_directory_uri(); ?>/img/footer-offices.png">
 					<ul class="footer-list">
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Blog & News</a></li>
-						<li><a href="#">FAQ</a></li>
-						<li><a href="#">Contact Us</a></li>
+
+						<?php
+
+						// check if the repeater field has rows of data
+						if( have_rows('links_menu', 'options') ):
+
+							// loop through the rows of data
+							while ( have_rows('links_menu', 'options') ) : the_row();
+
+								?>
+
+									<li><a href="<?php the_sub_field('url');?>"><?php the_sub_field('text');?></a></li>
+								<?php
+
+							endwhile;
+
+
+						endif;
+
+						?>
+						
 
 					</ul>
 				</div>
@@ -45,19 +64,33 @@
 					<img class="hidden" src="<?php echo get_template_directory_uri(); ?>/img/footer-offices.png">
 
 					<ul class="footer-list">
-						<li><a href="#">Golden Visa Portugal</a></li>
-						<li><a href="#">Golden Visa Spain</a></li>
-						<li><a href="#">Citizenship by Investment Cyprus</a></li>
-						<li><a href="#">Citizenship by Investment Malta</a></li>
-						<li><a href="#">Property Investment</a></li>
+					<?php
+
+						// check if the repeater field has rows of data
+						if( have_rows('links_menu_2', 'options') ):
+
+							// loop through the rows of data
+							while ( have_rows('links_menu_2', 'options') ) : the_row();
+
+								?>
+
+									<li><a href="<?php the_sub_field('url');?>"><?php the_sub_field('text');?></a></li>
+								<?php
+
+							endwhile;
+
+
+						endif;
+
+						?>
 					</ul>
 				</div>
 				<div class="social">
 					<img class="hidden" src="<?php echo get_template_directory_uri(); ?>/img/footer-offices.png">
 
-					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-linked.png"> <span>Connect with us on LinkedIn</span></a>
-					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-fb.png"> <span>Find us on Facebook</span></a>
-					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-ig.png"> <span>Find us on Instagram</span></a>
+					<?php $social_link_linked = get_field('social_links_linked', 'options'); if($social_link_linked) {  ?><a href="<?php the_field('social_links_linked', 'options');?>"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-linked.png"> <span>Connect with us on LinkedIn</span></a><?php } ?>
+					<?php $social_links_facebook = get_field('social_links-facebook', 'options'); if($social_links_facebook) {  ?><a href="<?php the_field('social_links-facebook', 'options');?>"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-fb.png"> <span>Find us on Facebook</span></a><?php } ?>
+					<?php $social_links_instagram_url = get_field('social_links-instagram_url', 'options'); if($social_links_instagram_url) {  ?><a href="<?php the_field('social_links-instagram_url', 'options');?>"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-ig.png"> <span>Find us on Instagram</span></a><?php } ?>
 				</div>
 			</div>
 		
