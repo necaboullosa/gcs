fabb0dbe46ed221edfe72250e0a00158b282afbc
Dmitry Christie <dmitry.christie@gmail.com>
2020-05-19 21:28:39 +0200

    mvoed copyright

--- footer.php
+++ footer.php
@@ -101,7 +101,7 @@
 		<hr>
 		<div class="copyright-outer">
 			<div class="copyright container">
-				<p>Global Citizen Solutions © 2017 -  <?php echo date("Y"); ?>
+				<p>Global Citizen Solutions  2017 -  <?php echo date("Y"); ?> ©
 . All rights reserved.</p>
 			</div>
 		</div>
