59cacacfd51178146c0e80cceb9dbf232dfcc26e
Dmitry Christie <dmitry.christie@gmail.com>
2020-05-21 18:57:08 +0200

    added back

--- footer.php
+++ footer.php
@@ -202,7 +202,7 @@ $('#toggle').toggle(
 
 
 </script>
-<!-- <script src="//instant.page/5.0.1" type="module" integrity="sha384-0DvoZ9kNcB36fWcQApIMIGQoTzoBDYTQ85e8nmsfFOGz4RHAdUhADqJt4k3K2uLS"></script> -->
+<script src="//instant.page/5.0.1" type="module" integrity="sha384-0DvoZ9kNcB36fWcQApIMIGQoTzoBDYTQ85e8nmsfFOGz4RHAdUhADqJt4k3K2uLS"></script> 
 
 </body>
 </html>
