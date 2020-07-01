1fe167d9c5ae79b95c4a21c6240b0ce2787c1ef7
Dmitry Christie <dmitry.christie@gmail.com>
2020-07-01 18:08:49 +0200

    ;

--- archive.php
+++ archive.php
@@ -18,7 +18,7 @@ get_header();
 			<header class="page-header">
 				<?php
 				echo '<h1 class="page-title">';
-				single_term_title() 
+				single_term_title(); 
 				echo '</h1>';
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
