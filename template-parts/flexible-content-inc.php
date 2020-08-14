3e2009c41ce3a94fa20bb8da093785b8cc59a34d
Dmitry Christie <dmitry.christie@gmail.com>
2020-08-14 16:59:15 +0200

    updated

--- template-parts/flexible-content-inc.php
+++ template-parts/flexible-content-inc.php
@@ -790,7 +790,9 @@ $k = 0;
                                                 "@type": "Answer",
                                                 "text": "<?php $answer = get_sub_field('answer'); 
                                                 $answer = preg_replace('style="font-weight: 400;"', '', $answer);
-                                                $answer = preg_replace(';', '', $answer);  
+                                                $answer = preg_replace(';', '', $answer); 
+                                                $answer = preg_replace('{', '', $answer);
+                                                $answer = preg_replace('}', '', $answer);    
                                                 echo $answer;
                                                 ?>"
                                                                 }
