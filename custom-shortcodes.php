169d1eb1b652e232716fe97c154a5e1eface1a72
necaboullosa <necaboullosa@gmail.com>
2020-10-28 16:06:31 +0000

    Update custom-shortcodes.php

---
+++ custom-shortcodes.php
@@ -0,0 +1,51 @@
+<?php
+ function hccmis_function() {
+     return '<a href ="https://quote.hccmis.com/atlastravel/?referid=28023&language=en-US">
+     HCCMIS Atlas Travel</a>';
+}
+add_shortcode('ins-hccmis', 'hccmis_function');
+
+ function axa_function() {
+     return '<a href ="https://www.awin1.com/awclick.php?gid=356196&mid=7970&awinaffid=642327&linkid=2599517&clickref=">
+     AXA Travel Insurance</a>';
+}
+add_shortcode('ins-axa', 'axa_function');
+
+ function cigna_function() {
+     return '<a href ="https://track.flexlinkspro.com/a.ashx?foid=1180188.143067092.I5298744&foc=16&fot=9999&fos=1">
+     Cigna International Healthcare</a>';
+}
+add_shortcode('ins-cigna', 'cigna_function');
+
+
+ function allianz_function() {
+     return '<a href ="https://www.jdoqocy.com/click-100219101-13378091">
+     Allianz Care International Health Insurance</a>';
+}
+add_shortcode('ins-allianz', 'allianz_function');
+
+
+ function schengen_function() {
+     return '<a href ="https://clk.tradedoubler.com/click?p=285301&a=3129216&g=24252672">
+     Europe Travel Schengen</a>';
+}
+add_shortcode('ins-schengen', 'schengen_function');
+
+
+
+ function worldnomads_function() {
+     return '<a href ="https://www.worldnomads.com/Turnstile/AffiliateLink?partnerCode=B3P0RTUG4L&source=link&utm_source=B3P0RTUG4L&utm_content=link&path=//www.worldnomads.com/travel-insurance/">
+     World Nomads Travel Insurance</a>';
+}
+add_shortcode('ins-worldnomads', 'worldnomads_function');
+
+
+
+ function hccmisstudent_function() {
+     return '<a href ="https://quote.hccmis.com/studentsecure/?referid=28023&language=en-US">
+     HCCMIS Student Insurance</a>';
+}
+add_shortcode('ins-hccmisstudent', 'hccmisstudent_function');
+
+
+?>
